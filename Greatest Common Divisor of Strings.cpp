10bcac7ec62ff60f68c50c2bf69ae62837632eae
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-02-01 12:38:22 +0530

    Create Greatest Common Divisor of Strings.cpp

---
+++ Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,18 @@
+// using HCF
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        return (str1 + str2 == str2+str1)?
+            str1.substr(0, gcd(str1.length(), str2.length())) : "";
+    }
+};
+
+
+/*
+LEETCODE 1071
+
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+*/
