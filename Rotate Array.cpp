a80bac0918d04c8a3ab0d72e119aabe8b0ff66a3
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-02-01 19:29:45 +0530

    Update Rotate Array.cpp

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
+    void swap(int&a,int&b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    
     void rotate(vector<int>& nums, int k) {
-        int x = k%nums.size();
+        //  1. 2. 3. 4. 5. 6. 7
+        // after 3 shifts
+        //  5. 6. 7. 1. 2. 3. 4
+        // solution
+        //. so basically-> we are reversing the whole array
+        //. 7. 6. 5. 4. 3. 2. 1
+        //  we are cutting it into two parts
+        // 0->k-1
+        //  7  6. 5     &  4  3. 2. 1
+        // are we are reversing both of these parts separtely
         
-        vector<int>res(nums.size());
-        
-        for(int i=0 ; i<nums.size()-x ; i++){
-            res[i+x] = nums[i]; 
+        // first reversal
+        k = k%nums.size();
+        int n = nums.size();
+        for(int i=0 ; i<n/2 ; i++) {
+            swap(nums[i], nums[n-i-1]);
         }
         
-        for(int i=nums.size()-x ; i<nums.size() ; i++){
-            res[i-nums.size()+x] = nums[i];
+        // reversal 2.0
+        for(int i=0 ; i<k/2 ; i++) {
+            swap(nums[i],nums[k-i-1]);
         }
         
-        for(int i=0 ; i<nums.size() ; i++){
-            nums[i] = res[i];
+        // reversal 3.0
+        for(int i=0 ; i<(n-k)/2 ; i++) {
+            swap(nums[i+k], nums[n-i-1]);
         }
     }
 };
 
 // LEETCODE 189
 // Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
-
+// in place algo
 
