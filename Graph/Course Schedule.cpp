bc63c10ab32aa91de5089635061c559cf50a9455
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-02-25 17:21:27 +0530

    Create Course Schedule.cpp

---
+++ Graph/Course Schedule.cpp
@@ -0,0 +1,63 @@
+
+// we are using the coloring algorithm
+
+class Solution {
+public:
+    
+    bool dfsUtils(vector<int>graph[],int V,vector<int>&vis,int s) {
+        if(vis[s] == 1) 
+            return false;
+        
+        if(vis[s] == 0)
+        {
+            vis[s] = 1;
+            for(auto x: graph[s]) {
+                bool res = dfsUtils(graph,V,vis,x);
+                if(res == false)
+                    return false;
+            }
+        }
+        
+        vis[s] = 2;
+        return true;
+    }
+    
+    bool dfs(vector<int>graph[], int V)
+    {
+        vector<int> vis(V,0);
+        for(int i=0 ; i<V ; i++) {
+            bool res = dfsUtils(graph,V,vis,i);
+            if(res == false) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+        
+        vector<int>graph[V];
+        for(auto x: prerequisites) {
+            int u = x[0], v = x[1];
+            graph[u].push_back(v);
+        }
+        
+        return dfs(graph,V);
+        
+        return true;
+    }
+};
+
+
+
+
+LEETCODE 207
+ 
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+
