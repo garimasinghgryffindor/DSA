36eceef621f83b8299ccd60577ece6e2a7c16f16
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-03-07 12:24:05 +0530

    Update Frog Jump code-studio.cpp

--- DP/Frog Jump code-studio.cpp
+++ DP/Frog Jump code-studio.cpp
@@ -1,3 +1,5 @@
+// Tabulation method
+
 #include <bits/stdc++.h> 
 int frogJump(int n, vector<int> &heights)
 {
@@ -18,4 +20,30 @@ int frogJump(int n, vector<int> &heights)
 
 
 Code studio
+
+
+
+// Space Optimization solution
+// like fibonaccii
+
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    // vector<int>dp(n,-1);
+    // dp[0] = 0;
+    int a = 0;
+    int b = 0, c;
+    for(int i=1 ; i<n ; i++) {
+        int first = a + abs(heights[i] - heights[i-1]);
+        int second = INT_MAX;
+        if(i>1) {
+            second = b + abs(heights[i] - heights[i-2]);
+        }
+        c = min(first, second);
+        b = a;
+        a = c;
+    }
+    return c;
+}
 
