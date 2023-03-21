dc7aa008c96f066465ca53348a1918a42e2bbbd1
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-03-21 17:54:25 +0530

    Update Maximum Profit in Job Scheduling.cpp

--- DP/1 D/Maximum Profit in Job Scheduling.cpp
+++ DP/1 D/Maximum Profit in Job Scheduling.cpp
@@ -3,20 +3,31 @@ public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
         vector<vector<int>> jobs;
-        for(int i=0 ; i<n ; i++){
-            jobs.push_back({startTime[i],endTime[i],profit[i]});
+        for(int i=0 ; i<n ; i++) {
+            jobs.push_back({endTime[i], startTime[i], profit[i]});
         }
-        sort(jobs.begin(),jobs.end());
-        vector<int>dp(n+1,0);
-        map<int,int>mp;
-        for(int i=n-1 ; i>=0 ; i--){
-            int st = jobs[i][0];
-            int end = jobs[i][1];
-            auto it = mp.lower_bound(end);
-            dp[i] = max(dp[i+1] , jobs[i][2]+(*it).second);
-            mp[st] = dp[i];
+        
+        sort(jobs.begin(), jobs.end());
+        
+        map<int,int> dp = {{0,0}};
+        //endTime, profit
+        
+        // we will go through all the jobs one by one.
+        
+        for(int i=0 ; i<n ; i++) {
+            auto job = jobs[i];
+            // profit in taking up the current job
+            // dp.upper_bound(x) will get the map iterator with key < current job starting time.
+            // and since we do not want any overlap we are going to take the previous job's total cumulative profit to add up to the current job profit
+            
+            int currProfit = prev(dp.upper_bound(job[1]))->second + job[2];
+            
+            if(currProfit > dp.rbegin()->second) {
+                dp[job[0]] = currProfit;
+            }
         }
-        return dp[0];
+        
+        return dp.rbegin()->second;
     }
-};
 
+};
