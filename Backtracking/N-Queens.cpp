679d00fc1e12650cfc1625d273db5598f0fdcc7b
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-05-15 16:40:02 +0530

    Create N-Queens.cpp

---
+++ Backtracking/N-Queens.cpp
@@ -0,0 +1,119 @@
+// BRUTE FORCE
+
+class Solution {
+public:
+    void recur(int n,set<vector<string> >&res,int j,vector<string>temp,int ans) {
+        if(ans == n) {
+            res.insert(temp);
+            return;
+        }
+        
+        for(int k=0 ; k<n ; k++) {
+                
+            int flag = 1;
+
+            int x = k;
+            int y = j;
+
+            // validation check over here for the index (x,y)
+            // checking rows and cols
+            for(int k=0 ; k<n ; k++) {
+                if(temp[x][k] == 'Q' || temp[k][y] == 'Q') {
+                    // not valid
+                    flag = 0;
+                    break;
+                }
+            }
+
+            // checking diagonal elements
+            int u = x, v = y;
+            // towards top-left
+            while(u>=0 && v>=0) {
+                if(temp[u][v] == 'Q') {
+                    // not valid
+                    flag = 0;
+                    break;
+                }
+                u--, v--;
+            }
+            u=x,v=y;
+            // towards bottom-right
+            while(u<n && v<n) {
+                if(temp[u][v] == 'Q') {
+                    // not valid
+                    flag = 0;
+                    break;
+                }
+                u++, v++;
+            }
+            u=x,v=y;
+            // towards top-right
+            while(u>=0 && u<n && v<n && v>=0) {
+                if(temp[u][v] == 'Q') {
+                    // not valid
+                    flag = 0;
+                    break;
+                }
+                u--;
+                v++;
+            }
+            u=x,v=y;
+            // towards top-left
+            while(u<n && u>=0 && v>=0 && v<n) {
+                if(temp[u][v] == 'Q') {
+                    // not valid
+                    flag = 0;
+                    break;
+                }
+                u++;
+                v--;
+            }
+            // checking diagonal elements ends here
+            
+
+            if(flag == 0)
+                continue;
+
+            // valid index otherwise
+            temp[x][y] = 'Q';
+            ans++;
+
+            if(ans == n) {
+                res.insert(temp);
+                continue;
+            }
+
+            // make a recursive function call
+            recur(n,res,y+1,temp,ans);
+
+            // backtrack
+            temp[x][y] = '.';
+            ans--;
+        }
+        
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        
+        if(n==1) {
+            return{{"Q"}};
+        }
+        
+        set<vector<string>> res;
+        
+        vector<string>temp(n);
+        for(int i=0 ; i<n ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                temp[i].push_back('.');
+            }
+        }
+        
+        int ans=0;
+        
+        recur(n,res,0,temp,ans);
+        
+        vector<vector<string>> res2(res.begin(),res.end());
+        
+        return res2;
+    }
+};
