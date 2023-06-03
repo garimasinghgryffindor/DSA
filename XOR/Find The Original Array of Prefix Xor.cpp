eb4115b423e499b37e4f1544caa71d51e982a044
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-06-03 12:44:19 +0530

    Update Find The Original Array of Prefix Xor.cpp

--- XOR/Find The Original Array of Prefix Xor.cpp
+++ XOR/Find The Original Array of Prefix Xor.cpp
@@ -3,12 +3,9 @@ public:
     vector<int> findArray(vector<int>& pref) {
         int n = pref.size();
         vector<int> res(n,0);
-        for(int i=n-1 ; i>=0 ; i--) {
-            if(i == 0) {
-                res[i] = pref[i];
-            } else {
-                res[i] = pref[i] ^ pref[i-1];
-            }
+        res[0] = pref[0];
+        for(int i=n-1 ; i>0 ; i--) {
+            res[i] = pref[i] ^ pref[i-1];
         }
         
         return res;
