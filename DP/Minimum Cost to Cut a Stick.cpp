6d91fa507a9220a3df969214b1291fdfeef596d7
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-08-08 10:09:57 +0530

    Update Minimum Cost to Cut a Stick.cpp

--- DP/Minimum Cost to Cut a Stick.cpp
+++ DP/Minimum Cost to Cut a Stick.cpp
@@ -56,3 +56,28 @@ public:
         return recur(0, n, 0, cuts.size() - 1);
     }
 };
+
+
+class Solution {
+public:   
+    // USING TABULATION
+    int dp[102][102];
+    
+    int minCost(int n, vector<int>& cuts) {
+        memset(dp, 0, sizeof(dp));
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        
+        for(int left=cuts.size()-1 ; left >=0 ; left--) {
+            for(int right=left+1 ; right < cuts.size() ; right++) {
+                for(int i = left+1; i < right; i++) {
+                    int costHere = cuts[right]-cuts[left] + dp[left][i] + dp[i][right];
+                    dp[left][right] = min(dp[left][right]==0?INT_MAX:dp[left][right], costHere);
+                }
+            }
+        }
+        
+        return dp[0][cuts.size() - 1];
+    }
+};
