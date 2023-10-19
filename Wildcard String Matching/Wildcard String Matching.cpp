9af44b678e2294a77448a89d99df771f708bf164
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-10-19 15:43:01 +0530

    Update Wildcard String Matching.cpp

--- Wildcard String Matching/Wildcard String Matching.cpp
+++ Wildcard String Matching/Wildcard String Matching.cpp
@@ -1,59 +1,29 @@
-// USING RECURSION
-bool recur(string&wild,string&pattern,int i, int j) {
-    if(i == wild.length() && j == pattern.length()) return true;
-    if(i > wild.length() || j > pattern.length()) return false;
-    
-    if(wild[i] == pattern[j]) return recur(wild,pattern,i+1,j+1);
-    
-    if(wild[i] == '?') return recur(wild,pattern,i+1,j+1);
-    if(wild[i] == '*') {
-        // either take none
-        if(recur(wild,pattern,i+1,j)) return true;
-        // or recursively take one
-        if(recur(wild, pattern, i+1,j+1)) return true;
-        // or recursively take many
-        return recur(wild,pattern,i,j+1);
+class Solution {
+public:
+    bool recur(string&s,string&p,int i,int j) {
+        if(i < 0 && j < 0) return true;
+        if(j < 0 && i >= 0) return false;
+        if(i < 0 && j >= 0) {
+            for(int k = 0; k <= j; k++) {
+                if(p[k] != '*') return false;
+            }
+            return true;
+        }
+        
+        if(s[i] == p[j] || p[j] == '?') return recur(s,p,i-1,j-1);
+        if(p[j] == '*') {
+            return recur(s,p,i-1,j) || recur(s,p,i,j-1);
+        }
+        return false;
     }
     
-    return false;
-}
-
-bool match(string wild, string pattern)
-{
-    return recur(wild, pattern, 0, 0);
-}
-
-
-// USING MEMOIZATION
-bool recur(vector<vector<int>>&mem, string&wild, string&pattern, int i, int j) {
-    if(mem[i][j] != -1) return mem[i][j];
-    
-    if(i == wild.length() && j == pattern.length()) {return mem[i][j] = true;}
-    if(i > wild.length() || j > pattern.length()) {return mem[i][j] = false;}
-    
-    if(wild[i] == pattern[j]) return mem[i][j] = recur(mem, wild,pattern,i+1,j+1);
-    
-    if(wild[i] == '?') return mem[i][j] = recur(mem, wild,pattern,i+1,j+1);
-    if(wild[i] == '*') {
-        // either take none
-        if(recur(mem, wild,pattern,i+1,j)) return mem[i][j] = true;
-        // or recursively take one
-        if(recur(mem, wild, pattern, i+1,j+1)) return mem[i][j] = true;
-        // or recursively take many
-        return mem[i][j] = recur(mem,wild,pattern,i,j+1);
+    bool isMatch(string s, string p) {
+        // using Recursion
+        int n = s.length(), m = p.length();
+        return recur(s,p,n-1,m-1);
     }
-    
-    return mem[i][j] = false;
-}
+};
 
-bool match(string wild, string pattern)
-{
-    vector<vector<int>>mem(wild.length()+2, vector<int>(pattern.length()+2, -1));
-    return recur(mem, wild, pattern, 0, 0);
-}
-
-
-// USING TABULATION
 
 
 
