3b3ab00f01341bd83ebd19349393f9d90eadbfc5
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-11-07 20:25:24 +0530

    Update Rotate Array.cpp

--- Array/Rotate Array.cpp
+++ Array/Rotate Array.cpp
@@ -9,3 +9,22 @@ public:
 };
 
 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        for(int start = 0; count < n; start++) {
+            int curr = start;
+            int prev = nums[start];
+            do {
+                int nxt = (curr+k)%n;
+                int temp = nums[nxt];
+                nums[nxt] = prev;
+                prev = temp;
+                curr = nxt;
+                count++;
+            } while(curr != start);
+        }
+    }
+};
