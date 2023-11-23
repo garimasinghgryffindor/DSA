bfa369c7fc108fc98d887c0e1400eda8872a8fb9
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-11-23 20:09:33 +0530

    Update 2305. Fair Distribution of Cookies.cpp

--- Bitmask/2305. Fair Distribution of Cookies.cpp
+++ Bitmask/2305. Fair Distribution of Cookies.cpp
@@ -55,3 +55,40 @@ public:
     }
 };
 
+
+
+// BITMASKING
+
+class Solution {
+public:
+    int distributeCookies(vector<int>& cookies, int k) {
+        int n = cookies.size();
+        vector<vector<int>>dp(k + 1, vector<int>(1ll << n, INT_MAX));
+        
+        vector<int>sum(1ll << n);
+        for(int mask = 0;mask<(1ll << n); mask++){
+            int total = 0;
+            for(int i = 0;i<n;i++){
+                if(mask & (1ll << i)){
+                    total += cookies[i];
+                }
+            }
+            sum[mask] = total;
+        }
+        
+        dp[0][0] = 0;
+        
+        
+        for(int person = 1; person <= k; person++) {
+            for(int mask = 0; mask < (1ll << n); mask++) {
+                for(int submask = mask; submask > 0; submask = (submask-1)&mask) {
+                    dp[person][mask] = min(dp[person][mask] , max(sum[submask], dp[person-1][submask^mask]));
+                }
+            }
+        }
+        
+        return dp[k][(1ll << n)-1];
+    }
+};
+
+
