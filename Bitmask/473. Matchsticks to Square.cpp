64bd6c1c26e1699261f212c772123b017253b2b2
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-11-26 20:46:31 +0530

    Update 473. Matchsticks to Square.cpp

--- Bitmask/473. Matchsticks to Square.cpp
+++ Bitmask/473. Matchsticks to Square.cpp
@@ -1,50 +1,37 @@
 // BRUTE FORCE  -----> TLE
-// class Solution {
-// public:
+class Solution {
+public:
     
-//     bool recur(unordered_map<int,int>mp, int idealLength, int n, int currLength) {
-//         if(n == 0) return true;
+    bool recur(vector<int>&vec, int idx, int s1, int s2, int s3, int s4) {
+        if(s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0) return true;
+        if(s1 < 0 || s2 < 0 || s3 < 0 || s4 < 0) return false;
         
-//         int reqLength = idealLength - currLength;
+        if(idx < 0) return false;
         
-//         for(auto x: mp) {
-//             int len = x.first, freq = x.second;
-//             if(freq) {
-//                 if(len < reqLength) {
-//                     mp[len]--;
-//                     if(recur(mp, idealLength, n-1, currLength + len)) return true;
-//                     mp[len]++;
-//                 } else if(len == reqLength) {
-//                     mp[len]--;
-//                     if(recur(mp, idealLength, n-1, 0)) return true;
-//                     mp[len]++;
-//                 }
-//             }
-//         }
+        if(recur(vec, idx-1, s1-vec[idx], s2, s3, s4) || recur(vec, idx-1, s1, s2-vec[idx], s3, s4) || recur(vec, idx-1, s1, s2, s3-vec[idx], s4) || recur(vec, idx-1, s1, s2, s3, s4-vec[idx])) return true;
         
-//         return false;               
-//     }
+        return false;               
+    }
     
-//     bool makesquare(vector<int>& matchsticks) {
-//         int peri = 0;
-//         unordered_map<int, int> freq;
+    bool makesquare(vector<int>& matchsticks) {
+        int peri = 0;
         
-//         for(int x: matchsticks) {
-//             peri += x;
-//             freq[x]++;
-//         }
+        for(int x: matchsticks) {
+            peri += x;
+        }
         
-//         sort(matchsticks.rbegin(), matchsticks.rend());
+        sort(matchsticks.rbegin(), matchsticks.rend());
         
-//         if(peri % 4 != 0) return false;
+        if(peri % 4 != 0) return false;
         
-//         int idealLength = peri / 4;
-//         if(matchsticks[0] > idealLength) return false;
+        int idealLength = peri / 4;
+        int n = matchsticks.size();
+        if(matchsticks[0] > idealLength) return false;
         
-//         return recur(freq, idealLength, matchsticks.size(), 0);
+        return recur(matchsticks, n-1, idealLength, idealLength, idealLength, idealLength);
         
-//     }
-// };
+    }
+};
 
 
 // BITMASK
