ff0e0ad16d218bf03315ca4ba5f1f00551342e7c
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-01-26 16:44:42 +0530

    Update 576. Out of Boundary Paths.cpp

--- DP/576. Out of Boundary Paths.cpp
+++ DP/576. Out of Boundary Paths.cpp
@@ -23,3 +23,39 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    // Approach 1
+    // using recursion
+    // Memoization
+    int mod = 1e9 + 7;
+    int dp[51][51][51];
+    
+    int recur(int m,int n,int maxMove,int startRow,int startColumn) {
+        // let us try using dfs
+        if(startRow <= -1 || startColumn <= -1 || startRow >= m || startColumn >= n) {
+            return 1;
+        }
+        
+        if(maxMove <= 0) return 0;
+        
+        if(dp[startRow][startColumn][maxMove] != -1) return dp[startRow][startColumn][maxMove];
+        
+        long sum = 0;
+        sum = (sum + recur(m, n, maxMove-1, startRow+1, startColumn)) % mod;
+        sum = (sum + recur(m, n, maxMove-1, startRow-1, startColumn)) % mod;
+        
+        sum = (sum + recur(m, n, maxMove-1, startRow, startColumn+1)) % mod;
+        sum = (sum + recur(m, n, maxMove-1, startRow, startColumn-1)) % mod;
+        
+        return dp[startRow][startColumn][maxMove] = (int)sum;
+    }
+    
+    
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memset(dp, -1, sizeof(dp));
+        return recur(m, n, maxMove, startRow, startColumn);
+    }
+    
+};
