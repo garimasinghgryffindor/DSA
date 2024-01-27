f4da1e6420be0f5eef0cf7a9d4bfc7bd73ab67b5
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-01-27 23:23:00 +0530

    Update 629. K Inverse Pairs Array.cpp

--- DP/629. K Inverse Pairs Array.cpp
+++ DP/629. K Inverse Pairs Array.cpp
@@ -23,6 +23,33 @@ public:
 
 
 // MEMOIZATION
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int dp[1001][1001];
+    
+    int recur(int n, int k) {
+        if(k <= 0) {
+            return k == 0;
+        }
+        if(n < 0) return 0;
+        
+        if(dp[n][k] != -1) return dp[n][k];
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            ans = (ans + recur(n-1, k-i))%mod;
+        }
+        return dp[n][k] = ans;
+    }
+    
+    int kInversePairs(int n, int k) {
+        memset(dp, -1, sizeof(dp));
+        return recur(n, k);
+    }
+};
+
+
 
 
 
