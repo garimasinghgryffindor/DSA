05976c780f1e77c6672642bc5ab7d84a8de41e4d
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-03-01 21:58:14 +0530

    Update 2864. Maximum Odd Binary Number.cpp

--- Greedy/2864. Maximum Odd Binary Number.cpp
+++ Greedy/2864. Maximum Odd Binary Number.cpp
@@ -10,3 +10,26 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {        
+        int mid = 0;
+        int l = 0;
+        
+        while(mid < s.length()) {
+            if(s[mid] == '1') {
+                swap(s[mid], s[l]);
+                l++;
+                mid++;
+            } else {
+                mid++;
+            }
+        }
+        
+        swap(s[l-1], s[s.length()-1]);
+        
+        return s;
+    }
+};
