81c413006cc815330aff760b28483004d88e2189
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-03-07 14:27:47 +0530

    Update 1044. Longest Duplicate Substring.cpp

--- Recursion/1044. Longest Duplicate Substring.cpp
+++ Recursion/1044. Longest Duplicate Substring.cpp
@@ -138,5 +138,84 @@ public:
 
 
 
+// approach 3
+// sliding window
+// rolling hash
+// rabin karp
+#define p 10000007
+
+class Solution {
+public:
+    vector<int> roll;
+
+    bool rk(string s, int len, string&temp) {
+        unordered_map<int, vector<int> > mp;
+        int d = 31;
+        int hash = 0;
+        
+        for(int i = 0; i < len; i++) {
+            hash = (hash*d + s[i]-'a')%p;
+        }
+        mp[hash].push_back(0);
+
+        for(int i = len; i < s.length(); i++) {
+            hash = ((hash - (s[i-len]-'a')*roll[len-1])%p + p )%p;
+            hash = (hash * d)%p;
+            hash = (hash + s[i]-'a')%p;
+            
+            if(mp.find(hash) != mp.end()) {
+                // duplicate value found
+                // let's recheck it for collision
+                for(auto x: mp[hash]) {
+                    string cur = s.substr(x, len);
+                    string curr = s.substr(i-len+1, len);
+                    if(cur.compare(curr)==0)
+                    {
+                        temp = curr;
+                        return true;
+                    }
+                }
+            }
+
+            mp[hash].push_back(i-len+1);
+        }
+
+        return false;
+    }
+    
+    string longestDupSubstring(string s) {
+        if(s.length() == 0) return "";
+        
+        roll.resize(s.length());
+        roll[0] = 1;
+        for(int i=1; i<s.length(); ++i)
+            roll[i] = (31*roll[i-1])%p;
+        
+        int l = 1, r = s.length();
+        string ans = "";
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            string temp = "";
+            
+            bool found = rk(s, mid, temp);
+            
+            if(found) {
+                if(temp.size() > ans.size()) {
+                    ans = temp;
+                }
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
+
 
 
