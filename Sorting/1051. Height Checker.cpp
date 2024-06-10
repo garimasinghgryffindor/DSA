fee75b288cc56c0f2995ec21d5cb119d96ae0431
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-06-10 21:09:15 +0530

    Create 1051. Height Checker.cpp

---
+++ Sorting/1051. Height Checker.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>vec(heights.begin(), heights.end());
+        sort(vec.begin(), vec.end());
+        int res = 0;
+        for(int i = 0; i < heights.size(); i++) {
+            if(heights[i] != vec[i]) res++;
+        }
+        return res;
+    }
+};
