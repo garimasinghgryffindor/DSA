24d51128f7253ab1064f31b77f63ea5218db6e96
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-06-10 21:24:35 +0530

    Update 1051. Height Checker.cpp

--- Sorting/1051. Height Checker.cpp
+++ Sorting/1051. Height Checker.cpp
@@ -14,5 +14,28 @@ public:
 
 
 // USING COUNTING SORT
-
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>vec;
+        int res = 0;
+        vector<int>freq(101, 0);
+        for(int x: heights) {
+            freq[x]++;
+        }
+        
+        int count = 0;
+        for(int i = 1; i <= 100; i++) {
+            int f = freq[i];
+            vector<int>temp(f, i);
+            copy(temp.begin(), temp.end(), back_inserter(vec));
+            count += f;
+        }
+        
+        for(int i = 0; i < heights.size(); i++) {
+            if(heights[i] != vec[i]) res++;
+        }
+        return res;
+    }
+};
 
