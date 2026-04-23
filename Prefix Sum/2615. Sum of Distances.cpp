fc33ff644085d135cb99054a25c7a13e05ed5936
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2026-04-23 22:54:17 +0530

    Create 2615. Sum of Distances.cpp

---
+++ Prefix Sum/2615. Sum of Distances.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long>res(n);
+        unordered_map<int, vector<int>> mp;
+
+        for(int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        for(auto iter: mp) {
+            int x = iter.first;
+            auto group = iter.second;
+            long long sum = 0;
+            for(int iter2: iter.second) {
+                sum += iter2;
+            }
+
+            long long leftSum = 0;
+            int groupsz = group.size();
+
+            for(int i = 0; i < groupsz; i++) {
+                long long rightSum = sum - leftSum - group[i];
+                long long left = 1LL * group[i] * i - leftSum;
+                long long right = rightSum - 1LL * group[i] * (groupsz - i - 1);
+
+                res[group[i]] = left + right;
+                leftSum += group[i];
+            }
+        }
+
+        return res;
+    }
+};
